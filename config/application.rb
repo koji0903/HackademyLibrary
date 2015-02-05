72f9f603a9b5dd7c6528211e3085a5c7c15fab59
Koji Hijikuro <koji0903@gmail.com>
2015-02-05 14:07:17 +0000

    Rails Startup Setting

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module HackademyLibrary
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.i18n.default_locale = :ja
   end
 end
